4187aab0dd4bd78930b1257d54a4c06fea7a56ec
ArisSpn_9 <arissopian0909@gmail.com>
2025-01-12 15:51:17 +0700

    finally

--- api/app.py
+++ api/app.py
@@ -1,21 +1,18 @@
 from flask import Flask, send_from_directory
 
-app = Flask(__name__, static_folder='src')
+app = Flask(__name__, static_folder='../src')
 
-# Endpoint untuk menyajikan file index.html
 @app.route('/')
 def serve_index():
     return send_from_directory(app.static_folder, 'index.html')
 
-# Endpoint untuk file statis di folder 'src' (CSS, JS, dll.)
 @app.route('/src/<path:path>')
 def serve_src(path):
     return send_from_directory(app.static_folder, path)
 
-# Endpoint untuk file di folder 'img'
 @app.route('/img/<path:path>')
 def serve_img(path):
-    return send_from_directory('img', path)
+    return send_from_directory('../img', path)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
