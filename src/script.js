58d174676a3043c609498a59e8535101b6c90f9c
ArisSpn_9 <arissopian0909@gmail.com>
2025-01-12 15:43:46 +0700

    last

--- src/script.js
+++ src/script.js
@@ -1,3 +1,26 @@
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll(".nav-list a");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach((section) => {
+        const sectionTop = section.offsetTop;
+        const sectionHeight = section.offsetHeight;
+        if (window.scrollY >= sectionTop - 50 && window.scrollY < sectionTop + sectionHeight) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach((link) => {
+        link.classList.remove("active");
+        if (link.getAttribute("href").includes(current)) {
+            link.classList.add("active");
+        }
+    });
+});
+
+
 const header = document.querySelector("header");
 
 window.addEventListener("scroll", () => {
